765f2a69f106a5c0228bc6140753c09e44ac98f2
suzinlim <122861956+suzinlim@users.noreply.github.com>
2023-08-07 00:57:57 +0900

    users model 수정

--- MOONAN/accounts/admin.py
+++ MOONAN/accounts/admin.py
@@ -1,62 +1,50 @@
 from django.contrib import admin
-from .models import UserInfo, Child, Parent, Medication, Supplement
+from .models import Child, Parent
+from users.models import User, Medication, Nutrition, UserMedication, UserNutrition
 
-class UserInfoAdmin(admin.ModelAdmin):
-    list_display = ('user_info', 'birthdate', 'gender', 'relationship', 'is_taking_meds', 'get_medication', 'get_supplement')
-
-    def get_medication(self, obj):
-        return ', '.join([str(med) for med in obj.medication.all()])
-    
-    def get_supplement(self, obj):
-        return ', '.join([str(s) for s in obj.supplement.all()])
-
-admin.site.register(UserInfo, UserInfoAdmin)
-admin.site.register(Medication)
-admin.site.register(Supplement)
-    
 class ChildAdmin(admin.ModelAdmin):
-    list_display = ('user_info', 'birthdate', 'gender', 'relationship', 'is_taking_meds', 'get_medication', 'get_supplement')
+    list_display = ('birthdate', 'gender', 'relationship', 'is_taking_meds', 'get_medications', 'get_nutritions')
 
     def birthdate(self, obj):
-        return obj.user_info.birthdate
+        return obj.user.birthdate
 
     def gender(self, obj):
-        return obj.user_info.gender
+        return obj.user.gender
 
     def relationship(self, obj):
-        return obj.user_info.relationship
+        return obj.user.relationship
     
     def is_taking_meds(self, obj):
-        return obj.user_info.is_taking_meds
+        return obj.user.is_taking_meds
     
-    def get_medication(self, obj):
-        return ', '.join([str(med) for med in obj.medication.all()])
-    
-    def get_supplement(self, obj):
-        return ', '.join([str(s) for s in obj.supplement.all()])
+    def get_medications(self, obj):
+        return ", ".join([medication.medication_name for medication in obj.medications.all()])
+
+    def get_nutritions(self, obj):
+        return ", ".join([nutrition.nutrition_name for nutrition in obj.nutritions.all()])
 
 
 admin.site.register(Child, ChildAdmin)
 
 class ParentAdmin(admin.ModelAdmin):
-    list_display = ('user_info', 'birthdate', 'gender', 'relationship', 'is_taking_meds', 'get_medication', 'get_supplement')
+    list_display = ('birthdate', 'gender', 'relationship', 'is_taking_meds', 'get_medications', 'get_nutritions')
 
     def birthdate(self, obj):
-        return obj.user_info.birthdate
+        return obj.user.birthdate
 
     def gender(self, obj):
-        return obj.user_info.gender
+        return obj.user.gender
 
     def relationship(self, obj):
-        return obj.user_info.relationship
+        return obj.user.relationship
     
     def is_taking_meds(self, obj):
-        return obj.user_info.is_taking_meds
+        return obj.user.is_taking_meds
 
-    def get_medication(self, obj):
-        return ', '.join([str(med) for med in obj.medication.all()])
-    
-    def get_supplement(self, obj):
-        return ', '.join([str(s) for s in obj.supplement.all()])
+    def get_medications(self, obj):
+        return ", ".join([medication.medication_name for medication in obj.medications.all()])
+
+    def get_nutritions(self, obj):
+        return ", ".join([nutrition.nutrition_name for nutrition in obj.nutritions.all()])
     
 admin.site.register(Parent, ParentAdmin)
--- MOONAN/accounts/models.py
+++ MOONAN/accounts/models.py
@@ -1,85 +1,21 @@
-from django.contrib.auth import get_user_model
 from django.db import models
-
+from django.contrib.auth import get_user_model
 User = get_user_model()
-
-class Medication(models.Model):
-    MEDICATION_CHOICES = [
-        ('blood_pressure', '혈압약'),
-        ('diabetes', '당뇨약'),
-        ('digestive', '소화제'),
-        ('cholesterol', '고지혈증약'),
-        ('constipation', '변비약'),
-        ('digestive_system', '위장약'),
-        ('antibiotics', '항생제'),
-        ('antihistamines', '항히스타민제'),
-        ('thyroid_disorder', '갑상선질환 치료제'),
-        ('gout_treatment', '통풍 치료제'),
-        ('anti-inflammatory', '소염 진통제'),
-        ('antidepressants', '우울증약'),
-        ('hormone_therapy', '성호르몬제'),
-        ('add', '직접 입력'),
-    ]
-    medication_name = models.CharField(max_length=100, choices=MEDICATION_CHOICES, verbose_name='복용 중인 약', unique=True)
-
-    def __str__(self):
-        return self.medication_name
-
-class Supplement(models.Model):
-    SUPPLEMENT_CHOICES = [
-        ('probiotics', '유산균'),
-        ('omega3', '오메가3'),
-        ('lutein', '루테인'),
-        ('multivitamin', '종합비타민'),
-        ('vitaminC', '비타민C'),
-        ('vitaminD', '비타민D'),
-        ('vitaminA', '비타민A'),
-        ('collagen', '콜라겐'),
-        ('iron', '철분'),
-        ('propolis', '프로폴리스'),
-        ('aronia', '아로니아'),
-        ('folic_acid', '엽산'),
-        ('magnesium', '마그네슘'),
-        ('vitaminE', '비타민E'),
-        ('calcium', '칼슘'),
-        ('add', '직접 입력'),
-    ]
-    supplement_name = models.CharField(max_length=100, choices=SUPPLEMENT_CHOICES, verbose_name='복용 중인 영양제', unique=True)
-
-    def __str__(self):
-        return self.supplement_name
-
-# 사용자의 공통 필드
-class UserInfo(models.Model):
-    user_info = models.OneToOneField(User, on_delete=models.CASCADE, related_name='user_info', verbose_name='사용자')
-    birthdate = models.DateField(verbose_name='생일')
-    GENDER_CHOICES = [
-        ('male', '남성'),
-        ('female', '여성'),
-    ]
-    gender = models.CharField(max_length=10, choices=GENDER_CHOICES, verbose_name='성별')
-    RELATIONSHIP_CHOICES = [
-        ('father', '아빠'),
-        ('mother', '엄마'),
-        ('daughter', '딸'),
-        ('son', '아들'),
-        ('grandmother', '할머니'),
-        ('grandfather', '할아버지'),
-        ('grandson', '손자'),
-        ('granddaughter', '손녀'),
-    ]
-    relationship = models.CharField(max_length=20, choices=RELATIONSHIP_CHOICES, verbose_name='소중한 분과의 관계')
-    is_taking_meds = models.BooleanField(default=False, verbose_name='복용 중인 약 및 영양제 여부')
-    medication = models.ManyToManyField(Medication, blank=True, related_name='medication')
-    supplement = models.ManyToManyField(Supplement, blank=True, related_name='supplement')
-
-    def __str__(self):
-        return f"{self.user_info.user_id}의 정보"
 
 class Child(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='children', null=True)
-    user_info = models.OneToOneField(UserInfo, on_delete=models.CASCADE, related_name='child')
 
+<<<<<<< Updated upstream
 class Parent(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='parents', null=True)
     user_info = models.OneToOneField(UserInfo, on_delete=models.CASCADE, related_name='parent')
+=======
+    def __str__(self):
+        return self.user.full_name
+
+class Parent(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='parents', null=True)
+
+    def __str__(self):
+        return self.user.full_name
+>>>>>>> Stashed changes
