1963ec1ef302e1e400afb8dc17e62d3f40a9cac6
gahyeon <1000ji1@naver.com>
2023-08-07 02:35:58 +0900

    connection template url 설정

--- MOONAN/config/urls.py
+++ MOONAN/config/urls.py
@@ -16,8 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+from config.views import ConnectView
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', ConnectView.as_view(), name='connection'),
     path('accounts/', include('accounts.urls', namespace='accounts')),
 ]
--- MOONAN/config/views.py
+++ MOONAN/config/views.py
@@ -3,4 +3,8 @@ from users.models import User
 from django.shortcuts import get_object_or_404, render, redirect
 
 class ConnectView(TemplateView):
-    temolate_name = 'connection.html'
+    template_name = 'connection.html'
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        # 추가적인 컨텍스트 데이터를 설정하고 싶다면 여기서 설정하세요.
+        return context
