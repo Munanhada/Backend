2bf4d817e205f24748a052dcd65c2a8cd6fbbd34
suzinlim <122861956+suzinlim@users.noreply.github.com>
2023-08-16 04:55:29 +0900

    양, 영양제 데이터 추가 가능 & UserMedication, UserNutrition 모델에 추가하는 거 진행중

--- MOONAN/static/javascript/home.js
+++ MOONAN/static/javascript/home.js
@@ -9,43 +9,24 @@ function updateMunanText(clickedListItem) {
     var munanTextElement = document.querySelector('.munan-list-text');
     munanTextElement.textContent = selectedText;
 
-    munanTextElement.style.color = "#FF003A"; // 선택지에 해당하는 색상 또는 기본 색상 적용
+    munanTextElement.style.color = "#FF003A";
     munanTextElement.style.fontWeight = "700";
 }
 
 function addList() {
     const addValue = document.getElementById('addValue').value;
 
-    if (addValue.trim() === '') {
-        alert('추가할 값을 입력하세요.');
-        return;
-    }
-
     const li = document.createElement("li");
-
     const textNode = document.createTextNode(addValue);
     li.appendChild(textNode);
 
     const ul = document.querySelector('.munan-menu-list-detail');
-    const referenceLi = document.querySelector('.munan-menu-list-detail li:nth-child(6)');
-    ul.insertBefore(li, referenceLi); // 오름차순으로 추가
+    const referenceLi = document.querySelector('.munan-menu-list-detail li:last-child');
+    ul.insertBefore(li, referenceLi);
 
     document.getElementById('addValue').value = '';
 
     li.addEventListener("click", function() {
         updateMunanText(this);
     });
-}
-
-// 드랍다운 보기 
-let reasonMenuBox = document.querySelector(".reasonMenu");
-function showdropdown(showImg) {
-    let dropdownMenuBox = showImg.nextElementSibling;
-
-    if(dropdownMenuBox.style.display === "none") {
-        dropdownMenuBox.style.display = "block";
-    }
-    else {
-        dropdownMenuBox.style.display = "none";
-    }
 }
