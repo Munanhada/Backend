a56651339dbb39693f0674d128c949d1cca3f8e4
gahyeon <1000ji1@naver.com>
2023-08-16 16:44:30 +0900

    connection 페이지 건너뛰기
    서버에서 이미 연결된사람 있거나, 상대방이 신청한 내역이 있으면 error_message를 보내는데 모달로 표시를 해야됨

--- MOONAN/accounts/views.py
+++ MOONAN/accounts/views.py
@@ -96,6 +96,7 @@ def login_view(request):
     # GET 요청할 경우, 로그인 HTML 응답
     return render(request, 'accounts/login.html')
 
+# 연결 요청
 @login_required
 def send_connection_request(request):
     if request.method =='POST':
@@ -103,7 +104,7 @@ def send_connection_request(request):
         to_user = request.POST.get('to_user')
         relationship1 = request.POST.get('relationship1')
         relationship2 = request.POST.get('relationship2')
-        
+        print(from_user,to_user, relationship1, relationship2 )
         try:
             to_user = get_user_model().objects.get(name=to_user)
         except get_user_model().DoesNotExist:
@@ -111,22 +112,22 @@ def send_connection_request(request):
             error_message = '연결 계정을 다시 한번 확인해주세요.'
             return render(request, 'accounts/accountConnection.html', {'error_message': error_message})
         else:
-                # 중복 신청 검사
-                existing_connection = ConnectionRequest.objects.filter(
-                Q(from_user=from_user, to_user=to_user) | Q(from_user=to_user, to_user=from_user)
-                )
-                if existing_connection.exists():
-                    error_message = '이미 연결 신청을 하셨습니다.'
-                    return render(request, 'accounts/accountConnection.html', {'error_message': error_message})
-                
-                connection_request = ConnectionRequest.objects.create(
-                    from_user=from_user,
-                    to_user=to_user,
-                    relationship1=relationship1,
-                    relationship2=relationship2,
-                )
-                # 필요한 후속 처리 (예: 연결 완료 메시지 표시)
-                return redirect('account:birth_info')  # 또는 적절한 리다이렉트 경로 설정
+            # 중복 신청 검사
+            existing_connection = ConnectionRequest.objects.filter(
+            Q(from_user=from_user, to_user=to_user) | Q(from_user=to_user, to_user=from_user)
+            )
+            if existing_connection.exists():
+                error_message = '이미 연결 신청을 하셨습니다.'
+                return render(request, 'accounts/accountConnection.html', {'error_message': error_message})
+            
+            connection_request = ConnectionRequest.objects.create(
+                from_user=from_user,
+                to_user=to_user,
+                relationship1=relationship1,
+                relationship2=relationship2,
+            )
+            # 필요한 후속 처리 (예: 연결 완료 메시지 표시)
+            return redirect('account:birth_info')  # 또는 적절한 리다이렉트 경로 설정
     else:
         return render(request, 'accounts/accountConnection.html')
     
