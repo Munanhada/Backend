96c067a25f35808691747b74ed7dc186f52c13b7
suzinlim <122861956+suzinlim@users.noreply.github.com>
2023-08-18 05:29:37 +0900

    메시지 전송

--- MOONAN/static/javascript/home.js
+++ MOONAN/static/javascript/home.js
@@ -4,17 +4,8 @@ overlapButton.addEventListener("click", function (event) {
     window.location.href = "/accountConnection.html"; // 페이지 이동
 });
 
-function updateMunanText(clickedListItem) {
-    var selectedText = clickedListItem.innerText;
-    var munanTextElement = document.querySelector('.munan-list-text');
-    munanTextElement.textContent = selectedText;
-
-    munanTextElement.style.color = "#FF003A";
-    munanTextElement.style.fontWeight = "700";
-}
-
 function addList() {
-    const addValue = document.getElementById('addValue').value;
+    const addValue = document.getElementById('customContentInput').value;
 
     const li = document.createElement("li");
     const textNode = document.createTextNode(addValue);
@@ -24,9 +15,46 @@ function addList() {
     const referenceLi = document.querySelector('.munan-menu-list-detail li:last-child');
     ul.insertBefore(li, referenceLi);
 
-    document.getElementById('addValue').value = '';
+    document.getElementById('customContentInput').value = '';
 
     li.addEventListener("click", function() {
         updateMunanText(this);
     });
+
+        // 추가한 내용을 customContent에 할당
+        const customContentInput = document.getElementById('customContentInput');
+        customContentInput.value = addValue;
+}
+
+// 드롭다운 메뉴 보이기/숨기기 함수
+function showdropdown(showImg) {
+    let dropdownMenuBox = showImg.nextElementSibling;
+
+    if (dropdownMenuBox) {
+        if(dropdownMenuBox.style.display === "none") {
+            dropdownMenuBox.style.display = "block";
+        } else {
+            dropdownMenuBox.style.display = "none";
+        }
+    } else {
+        console.log("Dropdown menu box not found.");
+    }
+}
+
+// 완료 버튼 눌렀을 때 팝업
+let savePopUp = document.getElementById("savePopUp");
+if (savePopUp) {
+    savePopUp.style.display = "none";
+}
+
+function showSavePopUp() {
+    if(savePopUp.style.display === "none") {
+        savePopUp.style.display = "flex";
+        $('#expressionForm').submit();
+    }
+}
+
+function closeSavePopUp() {
+    savePopUp.style.display = "none";
+    window.location.href = "/home";
 }
