a0a743654324906326fba50f94481d1a86812c7e
gahyeon <1000ji1@naver.com>
2023-08-18 02:37:11 +0900

    파일 분리

---
+++ MOONAN/config/settings/production.py
@@ -0,0 +1,33 @@
+# 운영환경
+
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False #운영하는 서버에서는 debug false
+
+ALLOWED_HOSTS = []
+
+DJANGO_APPS += [
+]
+
+PROJECT_APPS += [
+]
+
+THIRD_PARTY_APPS += [
+    
+]
+
+INSTALLED_APPS = DJANGO_APPS + PROJECT_APPS + THIRD_PARTY_APPS
+
+
+# Database
+# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+STATIC_ROOT = BASE_DIR / 'static'
