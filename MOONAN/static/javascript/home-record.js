fb4658a2fad70de203a119e0deb1ace53b1d4c90
drimh <107299318+drimh@users.noreply.github.com>
2023-08-17 16:27:37 +0900

    home 계정추가 버튼 url 수정, 이미지 변경 예정

--- MOONAN/static/javascript/home-record.js
+++ MOONAN/static/javascript/home-record.js
@@ -3,22 +3,27 @@ function mikeImgAlert() {
     alert("앱에서 확인해주세요.");
 }
 
-// let reasonOptionBox = document.querySelector(".reasonOptionBox");
+// 드롭다운 메뉴 보이기/숨기기 함수
 function showdropdown(showImg) {
     let dropdownMenuBox = showImg.nextElementSibling;
 
-    if(dropdownMenuBox.style.display === "none") {
-        dropdownMenuBox.style.display = "block";
-    }
-    else {
-        dropdownMenuBox.style.display = "none";
+    if (dropdownMenuBox) {
+        if(dropdownMenuBox.style.display === "none") {
+            dropdownMenuBox.style.display = "block";
+        } else {
+            dropdownMenuBox.style.display = "none";
+        }
+    } else {
+        console.log("Dropdown menu box not found.");
     }
 }
 
-
 // 완료 버튼 눌렀을 때 팝업
 let savePopUp = document.getElementById("savePopUp");
-savePopUp.style.display = "none";
+if (savePopUp) {
+    savePopUp.style.display = "none";
+}
+
 function showSavePopUp() {
     if(savePopUp.style.display === "none") {
         savePopUp.style.display = "flex";
@@ -33,12 +38,14 @@ function closeSavePopUp() {
 
 // 페이스 선택화면 숨기거나 보이기 
 let selectFace = document.querySelector(".selectReasonBox");
-selectFace.style.display = "none";
+if (selectFace) {
+    selectFace.style.display = "none";
+}
+
 function showReasonBox(emotion) {
     if(emotion === "sosoBox" || emotion === "badBox" || emotion === "worseBox") {
         selectFace.style.display = "flex";
-    }
-    else if (emotion === "goodBox" || emotion === "okayBox") {
+    } else if (emotion === "goodBox" || emotion === "okayBox") {
         selectFace.style.display = 'none';
     }
 } 
